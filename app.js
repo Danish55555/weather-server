8d81102a6e2b8122fc86dfd34fd127db5de94148
Danish Ali <danishali918841@gmail.com>
2023-12-09 21:17:37 +0500

    env setup with dotenv done

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.DEV_PORT || 5000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const weatherRouter = require("./routes/weatherRouter");
 const morgan = require('morgan');
 
-
-
 // Set up MQTT client
 require("./mqtt/mqtt");
+
+
 //Necessary Middlwares
 app.use(express.json({ limit: '2250mb' }));
 app.use(express.urlencoded({ limit: '2250mb', extended: 'true' }));
