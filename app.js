c522bf779136216177cd1be03c0e2ee76d1c25b1
Danish Ali <danishali918841@gmail.com>
2023-12-09 20:46:57 +0500

    feat: necessary packages added

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const mqtt = require('mqtt');
 const app = express();
 const port = 3000;
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+
 const mqttBrokerUrl = 'mqtt://localhost:1883'; // Replace with your VerneMQ broker address
 const weatherRouter=require("./routes/weatherRouter");
+const morgan = require('morgan')
+
+
+
 // Set up MQTT client
 const mqttClient = mqtt.connect(mqttBrokerUrl);
 
@@ -25,8 +33,14 @@ mqttClient.on('message', (topic, message) => {
 });
 
 
+app.use(express.json({ limit:'2250mb'})); 
+app.use(express.urlencoded({limit: '2250mb', extended: 'true'}));
 
 
+app.use(cors())
+
+app.use(cookieParser())
+app.use(morgan('dev'))
 app.get('/', (req, res) => {
   res.send('Welcome to the root URL');
 });
